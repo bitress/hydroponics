f1367c66995eeb4df7234daa344cbb8238910e92
bitress <cyannejustinl.vega12@gmail.com>
2025-04-08 14:58:53 +0800

    fixed missing icons

--- templates/head-styles.php
+++ templates/head-styles.php
@@ -17,7 +17,7 @@ href="https://cdn.jsdelivr.net/gh/zuramai/mazer@docs/demo/assets/compiled/css/ap
 rel="stylesheet"
 href="https://cdn.jsdelivr.net/gh/zuramai/mazer@docs/demo/assets/compiled/css/iconly.css"
 />
-<link rel="stylesheet" href="https://atugatran.github.io/FontAwesome6Pro/css/all.min.css" >
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/aquawolf04/font-awesome-pro@5cd1511/css/all.css" >
 
 <link rel="stylesheet" href="assets/css/custom.css" />
 <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" type="text/css">
